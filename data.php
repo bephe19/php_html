8a7a70dc1b657950c60a7a263c459a71de7788a2
bephe19 <bephe19@users.noreply.github.com>
2020-09-15 17:03:52 +0700

    commit pertama

---
+++ data.php
@@ -0,0 +1,36 @@
+<?php
+
+$data_siswa=[
+    "reyhan"=>["Muhammad Reyhan Rizki Ferdiyan","Bondowoso","24/05/2001",true],
+    "zainu"=>["Muhammad Zainuddin Basyar","Jepara","30/09/2003",false],
+    "ferdi"=>["Muhammad Ferdiansyah","Jepara","14/07/2003",false],
+    "reza1"=>["Reza Aditya Pratama","Jepara","04/04/2004",false],
+    "rimba"=>["Muhammad Rimba Abdillah","Jepara","02/07/2004",false],
+    "anang"=>["Anang Syah Amirul Haqim","Jepara","20/10/2003",false],
+    "yosafat"=>["Yosafat Restu Adi","Jepara","06/09/2003",false],
+    "bayu"=>["Raditya Fajar Bayu Pamungkas","Kendal","19/02/2003",false],
+    "yono"=>["Sudiyono","Kendal","10/05/2004",false],
+    "toba"=>["Toba Fathir Auladya","Kendal","04/04/2005",false],
+    "matsna"=>["Matsna Hidayatur Rochman","Magelang","01/05/2004",false],
+    "munif"=>["Munif Soleh","Magelang","18/01/2004",false],
+    "rendi"=>["Rendi Saputra","Magelang","01/04/2004",false],
+    "huda"=>["Ahmad Khaniful Huda","Magelang","21/04/2002",false],
+    "miftah"=>["Miftachul Rohman","Magelang","08/10/2003",false],
+    "dellas"=>["Aodellas Mangosiang","Semarang","14/03/2004",false],
+    "reza2"=>["Reza Andika Pratama","Sukorejo","08/06/2004",false],
+    "yuda"=>["Kusuma Yuda Mubarok","Sukorejo","25/07/2004",false],
+    "akbar"=>["Muhammad Akbar Kurnia","Sukorejo","07/03/2004",false],
+    "reza3"=>["Reza Fatikhul Hidayat","Sukorejo","06/08/2004",false],
+    "udin"=>["Khoirudin","Sukorejo","09/05/2003",false]
+];
+// foreach($data_siswa as $key=>$value)
+// {
+//     if($value[3])
+//     {
+//         echo "$value[0]\t sudah lulus \n";
+//     }else
+//     {
+//         echo "$value[0]\t belum lulus \n";
+//     }
+// };
+?>
---
+++ index.php
@@ -0,0 +1,36 @@
+<?php include "data.php";?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="asset/css/all.css">
+    <link rel="stylesheet" href="asset/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+
+    <?php foreach($data_siswa as $key=>$value): ?>
+
+        <div class="col-3">
+            <div class="card" style="width: 18rem;">
+             <img src="asset/images/rodok.jpg" class="card-img-top" alt="cards">
+            <div class="card-body">
+             <h5 class="card-title"><?php echo $value[0];?></h5>
+             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+             <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+            </div>
+        </div>
+
+<?php endforeach;?>
+        </div>
+    </div>
+
+    <script src="asset/js/jquery-3.5.1.min.js"></script>
+    <script src="asset/js/popper.js"></script>
+    <script src="asset/js/bootstrap.min.js"></script>
+</body>
+</html>
